0f78bbb198634678ca572b21156aa561c7ddd407
banansson <banansson@gmail.com>
2011-04-09 13:24:44 +0200

    user can request human readable output from df

--- df/Program.cs
+++ df/Program.cs
@@ -11,15 +11,26 @@ namespace NuTools.Df
 	{
 		public static int Main(string[] args)
 		{
+			var humanReadable = false;
+
 			var opts = new OptionParser();
 			opts.On("help", "display this help text and exit").Do(() => PrintUsageAndExit());
+			opts.On("human-readable", 'h', "print sizes in human readable format (e.g., 1K 234M 2G)").Do(() => humanReadable = true);
 			opts.Parse(args);
 
 			var driveRepository = new DriveRepository();
-			var driveSummary = new DriveSummary(driveRepository.GetDrives());
+			var driveSummary = CreateDriveSummary(driveRepository, humanReadable);
+
 			Console.Write(driveSummary.Render());
 
 			return 0;
+		}
+
+		private static DriveSummary CreateDriveSummary(DriveRepository driveRepository, bool humanReadable)
+		{
+			return humanReadable
+				? new DriveSummary(driveRepository.GetDrives(), new FileSizeFormatProvider())
+				: new DriveSummary(driveRepository.GetDrives());
 		}
 
 		private static void PrintUsageAndExit()
