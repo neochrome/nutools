7b0ee63e9e8badcecf38ec41bbee6292eeb19f6d
neochrome <johan@stenqvist.net>
2011-10-18 07:16:24 +0200

    added glob class

---
+++ common/Glob.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NuTools.Common
+{
+	public class Glob : List<string>
+	{
+		public Glob() : this(Environment.CurrentDirectory) { }
+		public Glob(string path) : this(path, new DirectoryWrapper()) { }
+
+		public Glob(string path, IDirectory directory)
+		{
+			this.path = path;
+			this.directory = directory;
+		}
+
+		public void Include(string pattern)
+		{
+			IncludeRecursive(path, pattern.Split(new[] { '/', '\\' }));
+		}
+
+		public void Exclude(string pattern)
+		{
+			var translate = new Dictionary<string, string> { { "*", @".*" } };
+			var expression = new Regex(@"^" + Regex.Replace(pattern, @"\*", match => translate[match.Value]) + @"$");
+			RemoveAll(f => expression.IsMatch(f));
+		}
+
+		private void IncludeRecursive(string path, string[] segments)
+		{
+			var pattern = segments.First();
+			var otherSegments = segments.Skip(1).ToArray();
+			if (otherSegments.Any())
+			{
+				directory
+					.EnumerateDirectories(path, pattern, pattern == "**" ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+					.Each(sub => IncludeRecursive(sub, otherSegments));
+			}
+			else
+			{
+				var files = directory.EnumerateFiles(path, pattern)
+					.Select(f => f.Replace(path, string.Empty).TrimStart('/', '\\'));
+				AddRange(files);
+			}
+		}
+
+		private readonly IDirectory directory;
+		private readonly string path;
+	}
+
+	public interface IDirectory
+	{
+		IEnumerable<string> EnumerateFiles(string path, string pattern);
+		IEnumerable<string> EnumerateDirectories(string path, string pattern, SearchOption searchOption);
+	}
+
+	public class DirectoryWrapper : IDirectory
+	{
+		public IEnumerable<string> EnumerateFiles(string path, string pattern)
+		{
+			return Directory.GetFiles(path, pattern);
+		}
+
+		public IEnumerable<string> EnumerateDirectories(string path, string pattern, SearchOption searchOption)
+		{
+			return Directory.GetDirectories(path, pattern, searchOption);
+		}
+	}
+}
---
+++ specs/Common/GlobSpecs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using FakeItEasy;
+using Cone;
+
+using NuTools.Common;
+
+namespace NuTools.Specs.Common
+{
+	[Describe(typeof(Glob))]
+	public class GlobSpecs
+	{
+		public void should_be_empty_when_created()
+		{
+			Verify.That(() => new Glob().Count == 0);
+		}
+
+		public void should_include_all_files_from_path()
+		{
+			var allFiles = new[] { "file1.ext", "file2.ext", "file3.ext" };
+			var directory = A.Fake<IDirectory>();
+			A.CallTo(directory).WithReturnType<IEnumerable<string>>().Returns(allFiles);
+			var target = new Glob(path, directory);
+
+			target.Include("*");
+
+			Verify.That(() => target.Count == allFiles.Length);
+		}
+
+		public void should_strip_path_from_included_file()
+		{
+			var directory = A.Fake<IDirectory>();
+			A.CallTo(directory).WithReturnType<IEnumerable<string>>().Returns(new[] { Path.Combine(path, "file.ext") });
+			var target = new Glob(path, directory);
+
+			target.Include("*");
+
+			Verify.That(() => target[0] == "file.ext");
+		}
+
+		public void should_include_files_from_path_matching_the_given_pattern()
+		{
+			var directory = A.Fake<IDirectory>();
+			var target = new Glob(path, directory);
+
+			target.Include("*.ext");
+
+			A.CallTo(() => directory.EnumerateFiles(path, "*.ext")).MustHaveHappened();
+		}
+
+		public void should_include_files_from_sub_path_in_the_given_pattern()
+		{
+			var subPath = Path.Combine(path, "sub");
+			var directory = A.Fake<IDirectory>();
+			A.CallTo(() => directory.EnumerateDirectories(null, null, SearchOption.AllDirectories))
+				.WithAnyArguments().Returns(new[] { subPath });
+			var target = new Glob(path, directory);
+
+			target.Include("sub/*.ext");
+
+			A.CallTo(() => directory.EnumerateDirectories(path, "sub", SearchOption.TopDirectoryOnly)).MustHaveHappened();
+			A.CallTo(() => directory.EnumerateFiles(subPath, "*.ext")).MustHaveHappened();
+		}
+
+		public void should_include_files_from_sub_path_at_any_depth()
+		{
+			var subPath = Path.Combine(Path.Combine(path, "any"), "sub");
+			var directory = A.Fake<IDirectory>();
+			A.CallTo(() => directory.EnumerateDirectories(null, null, SearchOption.AllDirectories))
+				.WithAnyArguments().Returns(new[] { subPath });
+			var target = new Glob(path, directory);
+
+			target.Include("**/sub/*.txt");
+
+			A.CallTo(() => directory.EnumerateDirectories(path, "**", SearchOption.AllDirectories)).MustHaveHappened();
+			A.CallTo(() => directory.EnumerateFiles(subPath, "*.txt")).MustHaveHappened();
+		}
+
+		public void should_exclude_file_matching_pattern()
+		{
+			var target = new Glob { "file1.ext", "file2.ext2" };
+
+			target.Exclude("file1.ext");
+
+			Verify.That(() => target.SequenceEqual(new[] { "file2.ext2" }));
+		}
+
+		public void should_exclude_all_files_matching_pattern()
+		{
+			var target = new Glob { "file1.ext", "file2.ext", "file3.doc" };
+
+			target.Exclude("*.ext");
+
+			Verify.That(() => target.SequenceEqual(new[] { "file3.doc" }));
+		}
+
+		const string path = "path";
+	}
+}
