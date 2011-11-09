d87eda957f171531c25ee16324df67747e26a501
banansson <banansson@gmail.com>
2011-11-09 11:14:13 +0100

    df: added missing options text

--- df/Program.cs
+++ df/Program.cs
@@ -20,18 +20,28 @@ namespace NuTools.Df
 
 			var opts = new OptionParser();
 			opts.Header = "Show information about the file system on which each FILE resides,\nor all file systems by deafult.";
-			opts.On("help", "display this help text and exit").Do(() => {
+
+            opts.Args<string>("FILE", "").Do(files => { });
+
+            opts.On("help", "display this help text and exit").Do(() => {
 				opts.WriteUsage(Console.Out);
 				Environment.Exit(0);
 			});
 			opts.On("human-readable", 'h', "print sizes in human readable format (e.g., 1K 234M 2G)").Do(() => humanReadable = true);
 		    opts.On("print-type", 'T', "print file system type").Do(() => printFileSystemType = true);
             opts.On("type", 't', "limit listing to file systems of type TYPE").WithArg<string>("TYPE").Do(arg => limitToType = arg);
-            opts.On("version", 'V', "print version information and exit").Do(() =>
+            opts.On("version", "print version information and exit").Do(() =>
 			{
 				opts.WriteVersionInfo(Console.Out);
 				Environment.Exit(0);
 			});
+
+		    opts.Footer = "Display values are in units of the first available SIZE from --block-size,\n";
+		    opts.Footer += "and the DF_BLOCK_SIZE, BLOCK_SIZE and BLOCKSIZE environment variables.\n";
+		    opts.Footer += "Otherwise, units default to 1024 bytes (or 512 if POSIXLY_CORRECT is set).\n";
+		    opts.Footer += "\n";
+		    opts.Footer += "SIZE may be (or may be an integer optionally followed by) one of following:\n";
+		    opts.Footer += "KB 1000, K 1024, MB 1000*1000, M 1024*1024, and so on for G, T, P, E, Z, Y.\n";
 			
             if (!opts.Parse(args))
             {
