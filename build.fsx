a560dc923c7568b1bdbaeccc8addb63d8c0af9ea
neochrome <johan@stenqvist.net>
2012-01-04 07:29:29 +0100

    added bash script to build

--- build.fsx
+++
@@ -1,7 +0,0 @@
-﻿#load @"lib\fake.fsx"
-
-build("nutools.sln") 
-&& merge_exe(@"bin\grep.exe", [@"bin\grep.exe";@"bin\common.dll"])
-&& merge_exe(@"bin\sed.exe", [@"bin\sed.exe";@"bin\common.dll"])
-&& merge_exe(@"bin\df.exe", [@"bin\df.exe";@"bin\common.dll"])
-&& package(@"bin\nutools.zip", [@"bin\grep.exe";@"bin\sed.exe";@"bin\df.exe"])
